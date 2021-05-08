fbf9599c5b98f3c5901c2180a004eee46456ff43
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-05-08 20:54:37 +0900

    関数追加

--- Week1/ex2.cpp
+++ Week1/ex2.cpp
@@ -2,14 +2,21 @@
 #include "utils.h"
 using namespace std;
 
-vector<vector<string>> createNewDictionary(int length);
 vector<string> findAnagram(string inputPath);
+vector<vector<string>> createNewDictionary(int length);
+string findBestAnagram(vector<string> results);
 int calculateScore(string word);
 
 const vector<string> dictionary = loadFile("./anagram/words.txt");
 
 int main() {
 
+}
+
+/** 入力に合わせたアナグラムを検索 */
+vector<string> findAnagram(string inputPath) {
+  vector<string> inputs = loadFile(inputPath);
+  vector<vector<string>> newDictionary = createNewDictionary(inputs[0].length());
 }
 
 /** 入力された単語の長さに合わせて新辞書作成 */
@@ -22,13 +29,22 @@ vector<vector<string>> createNewDictionary(int length) {
     std::sort(str.begin(), str.end());
     newDictionary.push_back({ str, dictionary[i] });
   }
+  sort(newDictionary.begin(), newDictionary.end(),[](const vector<string> &alpha, const vector<string> &beta){return alpha[0] < beta[0];});
   return newDictionary;
 }
 
-/** 入力に合わせたアナグラムを検索 */
-vector<string> findAnagram(string inputPath) {
-  vector<string> inputs = loadFile(inputPath);
-  vector<vector<string>> newDictionary = createNewDictionary(inputs[0].length());
+/** 見つかったアナグラムの中からベストスコアのものを抽出 */
+string findBestAnagram(vector<string> results) {
+  string ans;
+  int bestScore = 0;
+  for (int i = 0; i < results.size(); i++)
+  {
+    if(calculateScore(results[i]) > bestScore) {
+      ans = results[i];
+      bestScore = calculateScore(results[i]);
+    }
+  }
+  return ans;
 }
 
 /** score_checker.py の calculate_score を移植 */
