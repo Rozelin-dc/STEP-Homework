679f3e0181ba0129089b6117fed5243aaa41e7a3
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-05-08 11:56:20 +0900

    Update ex1.cpp

--- Week1/ex1.cpp
+++ Week1/ex1.cpp
@@ -1,21 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void loadDictionary(vector<string> dictionary);
+vector<string> loadDictionary();
+vector<vector<string>> createNewDictionary(vector<string> dictionary, int length);
 
 int main() {
-  vector<string> dictionary(0);
-  loadDictionary(dictionary);
+  vector<string> dictionary = loadDictionary();
+  for (int i = 0; i < 10; i++)
+  {
+    cout<<dictionary[i]<<endl;
+  }
+  string input;
+  cin>>input;
+  vector<vector<string>> newDictionary = createNewDictionary(dictionary, input.length());
+  for (int i = 0; i < 10; i++)
+  {
+    cout<<newDictionary[i][0]<<" "<<newDictionary[i][1]<<endl;
+  }
+  
 }
 
-void loadDictionary(vector<string> dictionary) {
+vector<string> loadDictionary() {
   std::ifstream ifs("./anagram/words.txt");
   if (ifs.fail()) {
     std::cerr << "Failed to open file." << std::endl;
-    return;
+    return {"error"};
   }
   string str;
-  while (getline(ifs, str)) {
+  vector<string> dictionary(0);
+  while (std::getline(ifs, str)) {
     dictionary.push_back(str);
   }
+  return dictionary;
+}
+
+vector<vector<string>> createNewDictionary(vector<string> dictionary, int length) {
+  vector<vector<string>> newDictionary(0, vector<string>(2));
+  for (int i = 0; i < dictionary.size(); i++)
+  {
+    if (dictionary[i].length() != length) continue;
+    string str = dictionary[i];
+    std::sort(str.begin(), str.end());
+    newDictionary.push_back({ str, dictionary[i] });
+  }
+  return newDictionary;
 }
