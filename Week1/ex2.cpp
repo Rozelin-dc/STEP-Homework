76211b3ac8b74a5477d348f0f4f5db2b3f5fd55f
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-05-09 10:58:34 +0900

    計算量度外視のアルゴリズムで完成

--- Week1/ex2.cpp
+++ Week1/ex2.cpp
@@ -2,28 +2,47 @@
 #include "utils.h"
 using namespace std;
 
-vector<string> findAnagram(string inputPath);
-vector<vector<string>> createNewDictionary(int length);
-vector<string> search(string word, vector<vector<string>> newDictionary);
+vector<string> findAnagrams(string inputPath);
+void createNewDictionary(int length);
+vector<string> search(string word);
+bool isSubset(vector<int> inputMap, vector<int> wordMap);
 string findBestAnagram(vector<string> anagrams);
 int calculateScore(string word);
+void output(char *fileName, vector<string> contents);
 
 const vector<string> dictionary = loadFile("./anagram/words.txt");
+vector<string> newDictionary(0);
+vector<vector<int>> wordsMap(0, vector<int>(26));
 
 int main() {
+  vector<string> ans;
+  char *outputPath;
+  
+  outputPath = "./anagram/small-answer.txt";
+  ans = findAnagrams("./anagram/small.txt");
+  output(outputPath, ans);
 
+  outputPath = "./anagram/medium-answer.txt";
+  ans = findAnagrams("./anagram/medium.txt");
+  output(outputPath, ans);
+
+  outputPath = "./anagram/large-answer.txt";
+  ans = findAnagrams("./anagram/large.txt");
+  output(outputPath, ans);
+  
+  return 0;
 }
 
 /** 入力に合わせたアナグラムを検索 */
-vector<string> findAnagram(string inputPath) {
+vector<string> findAnagrams(string inputPath) {
   vector<string> inputs = loadFile(inputPath);
-  vector<vector<string>> newDictionary = createNewDictionary(inputs[0].length());
+  createNewDictionary(inputs[0].length());
 
   vector<string> ans(0);
 
-  for (int i = 0; i < inputs.size(); i++)
+  for (int i = 0; i < (int)inputs.size(); i++)
   {
-    vector<string> anagrams = search(inputs[i], newDictionary);
+    vector<string> anagrams = search(inputs[i]);
     string bestAnagram = findBestAnagram(anagrams);
     ans.push_back(bestAnagram);
   }
@@ -31,28 +50,54 @@ vector<string> findAnagram(string inputPath) {
 }
 
 /** 入力された単語の長さに合わせて新辞書作成 */
-vector<vector<string>> createNewDictionary(int length) {
-  vector<vector<string>> newDictionary(0, vector<string>(2));
+void createNewDictionary(int length) {
   for (int i = 0; i < (int)dictionary.size(); i++)
   {
     if ((int)dictionary[i].length() > length) continue;
+
     string str = dictionary[i];
-    std::sort(str.begin(), str.end());
-    newDictionary.push_back({ str, dictionary[i] });
+    newDictionary.push_back({ str });
+
+    vector<int> wordMap(26);
+    for (int i = 0; i < (int)str.length(); i++)
+    {
+      wordMap[str[i] - 'a'] += 1;
+    }
+    wordsMap.push_back(wordMap);
   }
-  sort(newDictionary.begin(), newDictionary.end(), [](const vector<string> &alpha, const vector<string> &beta){return alpha[0] < beta[0];});
-  return newDictionary;
+  return;
 }
 
-vector<string> search(string word, vector<vector<string>> newDictionary) {
+vector<string> search(string word) {
+  vector<int> inputMap(26);
+  for (int i = 0; i < (int)word.length(); i++)
+  {
+    inputMap[word[i] - 'a'] += 1;
+  }
 
+  vector<string> ans(0);
+
+  for (int i = 0; i < (int)newDictionary.size(); i++)
+  {
+    if(isSubset(inputMap, wordsMap[i])) ans.push_back(newDictionary[i]);
+  }
+  return ans;
+}
+
+/** 第二引数の配列が第一引数の配列のサブセットになっているか */
+bool isSubset(vector<int> inputMap, vector<int> wordMap) {
+  for (int i = 0; i < 26; i++)
+  {
+    if(inputMap[i] < wordMap[i]) return false;
+  }
+  return true;
 }
 
 /** 見つかったアナグラムの中からベストスコアのものを抽出 */
 string findBestAnagram(vector<string> anagrams) {
   string ans;
   int bestScore = 0;
-  for (int i = 0; i < anagrams.size(); i++)
+  for (int i = 0; i < (int)anagrams.size(); i++)
   {
     if(calculateScore(anagrams[i]) > bestScore) {
       ans = anagrams[i];
@@ -66,9 +111,25 @@ string findBestAnagram(vector<string> anagrams) {
 int calculateScore(string word) {
   const int scores[] = { 1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4 };
   int score = 0;
-  for (int i = 0; i < word.length(); i++)
+  for (int i = 0; i < (int)word.length(); i++)
   {
     score += scores[word[i] - 'a'];
   }
   return score;
+}
+
+/** 外部ファイルへの書き出し */
+void output(char *fileName, vector<string> contents) {
+  std::ofstream ofs(fileName);
+
+  if (!ofs) {
+    cout<< "ファイルが開けませんでした。" <<endl;
+    return;
+  }
+
+  for (int i = 0; i < (int)contents.size(); i++)
+  {
+    ofs<<contents[i]<<endl;
+  }
+  return;
 }
