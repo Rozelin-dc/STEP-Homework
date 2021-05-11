96d4eb950101ab98919977c9846fc3500bdd0962
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-05-11 09:12:01 +0900

    fix ex1

--- Week1/ex1.cpp
+++ Week1/ex1.cpp
@@ -2,8 +2,8 @@
 #include "utils.h"
 using namespace std;
 
-vector<vector<string>> createNewDictionary(int length);
-int search(string word, vector<vector<string>> newDictionary);
+vector<vector<string>> createDictionaryWithLength(int length);
+int findIndexOfAnagram(string word, vector<vector<string>> newDictionary);
 
 const vector<string> dictionary = loadFile("./anagram/words.txt");
 
@@ -11,22 +11,24 @@ int main() {
   string input;
   cin>>input;
 
-  vector<vector<string>> newDictionary = createNewDictionary(input.length());
+  vector<vector<string>> newDictionary = createDictionaryWithLength(input.length());
 
   string sortedInput = input;
   std::sort(sortedInput.begin(), sortedInput.end());
-  int findIdx = search(sortedInput, newDictionary);
+  int findIdx = findIndexOfAnagram(sortedInput, newDictionary);
 
   vector<string> ans(0);
-  int idx = findIdx;
-  while(newDictionary[idx][0] == sortedInput) {
-    ans.push_back(newDictionary[idx][1]);
-    idx--;
-  }
-  idx = findIdx + 1;
-  while(newDictionary[idx][0] == sortedInput) {
-    ans.push_back(newDictionary[idx][1]);
-    idx++;
+  if(findIdx != -1) {
+    int idx = findIdx;
+    while(newDictionary[idx][0] == sortedInput) {
+      ans.push_back(newDictionary[idx][1]);
+      idx--;
+    }
+    idx = findIdx + 1;
+    while(newDictionary[idx][0] == sortedInput) {
+      ans.push_back(newDictionary[idx][1]);
+      idx++;
+    }
   }
 
   for (int i = 0; i < (int)ans.size(); i++)
@@ -37,40 +39,35 @@ int main() {
 }
 
 /** 入力された単語の長さに合わせて新辞書作成 */
-vector<vector<string>> createNewDictionary(int length) {
+vector<vector<string>> createDictionaryWithLength(int length) {
   vector<vector<string>> newDictionary(0, vector<string>(2));
   for (int i = 0; i < (int)dictionary.size(); i++)
   {
     if ((int)dictionary[i].length() != length) continue;
+
     string str = dictionary[i];
     std::sort(str.begin(), str.end());
     newDictionary.push_back({ str, dictionary[i] });
   }
+  // ソートされた単語を元にソート
   sort(newDictionary.begin(), newDictionary.end(),[](const vector<string> &alpha, const vector<string> &beta){return alpha[0] < beta[0];});
   return newDictionary;
 }
 
 /** アナグラム検索 */
-int search(string word, vector<vector<string>> newDictionary) {
-  bool found = false;
+int findIndexOfAnagram(string word, vector<vector<string>> newDictionary) {
   int beginIdx = 0;
-  int endIdx = (int)newDictionary.size();
+  int endIdx = (int)newDictionary.size() - 1;
   int searchIdx = (endIdx - beginIdx) / 2;
-  while(!found) {
+  while(beginIdx <= endIdx) {
     searchIdx = ((endIdx - beginIdx) / 2) + beginIdx;
 
     if(newDictionary[searchIdx][0] == word) {
-      found = true;
       return searchIdx;
     } else if(word.compare(newDictionary[searchIdx][0]) < 0) {
       endIdx = searchIdx - 1;
     } else {
       beginIdx = searchIdx + 1;
-    }
-
-    if(endIdx <= beginIdx) {
-      searchIdx = endIdx;
-      return searchIdx;
     }
   }
   return -1;
