013f92bcb521312f77514deec83590dba71a2b0e
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-05-15 10:34:22 +0900

    fix
    乱数の生成方法と計測ポイントを変更

--- Week2/ex1.cpp
+++ Week2/ex1.cpp
@@ -1,30 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> makeMatrix(int size);
+#define MIN 0
+#define MAX 100
+
+vector<vector<int>> makeMatrix(int size, int seed);
 vector<vector<int>> multMatrixes(int size, vector<vector<int>> a, vector<vector<int>> b);
 void printMatrix(int size, vector<vector<int>> output);
 
 int main() {
-  clock_t start = clock(); // 計測開始
-
   int n;
   cin>>n;
-  vector<vector<int>> a = makeMatrix(n);
-  vector<vector<int>> b = makeMatrix(n);
+
+  // 行列生成
+  std::random_device rnd;
+  vector<vector<int>> a = makeMatrix(n, rnd());
+  vector<vector<int>> b = makeMatrix(n, rnd());
+
+  clock_t start = clock(); // 計測開始
+
   vector<vector<int>> ans = multMatrixes(n, a, b);
 
   clock_t end = clock(); // 計測終了
 
   // デバッグ用出力
-  printMatrix(n, a);
-  cout<<endl;
-  printMatrix(n, b);
-  cout<<endl;
-  printMatrix(n, ans);
+  // printMatrix(n, a);
+  // cout<<endl;
+  // printMatrix(n, b);
+  // cout<<endl;
+  // printMatrix(n, ans);
 
   // 結果を外部ファイルに出力
-  const char fileName[] = "result.csv";
+  const char fileName[] = "ex1-result.csv";
   std::ofstream ofs(fileName, ios::app);
 
   if (!ofs) {
@@ -32,16 +39,19 @@ int main() {
     return 1;
   }
   ofs<<n<<","<<(double)(end - start) / CLOCKS_PER_SEC<<endl;
+  return 0;
 }
 
-vector<vector<int>> makeMatrix(int size) {
+vector<vector<int>> makeMatrix(int size, int seed) {
   vector<vector<int>> matrix(size, vector<int>(size));
-  std::random_device random;
+  std::default_random_engine eng(seed);
+  std::uniform_int_distribution<int> distr(MIN, MAX);
+
   for (int i = 0; i < size; i++)
   {
     for (int j = 0; j < size; j++)
     {
-      matrix[i][j] = random();
+      matrix[i][j] = distr(eng);
     }
   }
   return matrix;
@@ -68,7 +78,7 @@ void printMatrix(int size, vector<vector<int>> output) {
   {
     for (int j = 0; j < size; j++)
     {
-      cout<<output[i][j];
+      cout<<output[i][j]<<" ";
     }
     cout<<endl;
   }
